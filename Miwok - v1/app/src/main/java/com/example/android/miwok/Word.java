c1ec0e24b2fa53f66bf749665f66f5d8272762d5
christian <christianesl@gmail.com>
2018-03-04 16:37:59 -0600

    final miwok version

---
+++ Miwok - v1/app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,66 @@
+package com.example.android.miwok;
+
+/**
+ * {@link Word} represents a vocabulary word that the user wants to learn.
+ * It contains a default translation and a Miwok translation for that word.
+ */
+
+public class Word {
+    /**
+     * @param mDefaultTranslation default english word.
+     * @param mWiwokTranslation translation into miwok language.
+     * @param mImageResourceId image.
+     */
+    private String mDefaultTranslation;
+    private String mWiwokTranslation;
+
+    //audio resource
+    private int mAudioResourceId;
+
+    //image resource
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+    private static final int NO_IMAGE_PROVIDED = -1;
+
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mWiwokTranslation = miwokTranslation;
+        mAudioResourceId = audioResourceId;
+    }
+
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId, int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mWiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResourceId;
+    }
+
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    public String getMiwokTranslation() {
+        return mWiwokTranslation;
+    }
+
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+
+    public int getAudioResourceId() {
+        return mAudioResourceId;
+    }
+
+    @Override
+    public String toString() {
+        return "Word{" +
+                "mDefaultTranslation='" + mDefaultTranslation + '\'' +
+                ", mWiwokTranslation='" + mWiwokTranslation + '\'' +
+                ", mAudioResourceId=" + mAudioResourceId +
+                ", mImageResourceId=" + mImageResourceId +
+                '}';
+    }
+}
--- Miwok/app/src/main/java/com/example/android/miwok/MainActivity.java
+++ Miwok/app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -1,10 +1,24 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.android.miwok;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,59 +29,23 @@ public class MainActivity extends AppCompatActivity {
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
-        //Find the View that shows the numbers category
-        TextView numbers = (TextView)findViewById(R.id.numbers);
-        //Set clickListener on that View
-        numbers.setOnClickListener(new View.OnClickListener(){
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
 
-            @Override
-            public void onClick(View view){
-                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
-                startActivity(numbersIntent);
-            }
-        });
+        // Create an adapter that knows which fragment should be shown on each page
+        CategoryAdapter adapter = new CategoryAdapter(this, getSupportFragmentManager());
 
-        TextView family = (TextView)findViewById(R.id.family);
-        //Set clickListener on that View
-        family.setOnClickListener(new View.OnClickListener(){
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
 
-            @Override
-            public void onClick(View view){
-                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
-                startActivity(familyIntent);
-            }
-        });
+        // Find the tab layout that shows the tabs
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
 
-
-        TextView colors = (TextView)findViewById(R.id.colors);
-        //Set clickListener on that View
-        colors.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View view){
-                Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
-                startActivity(colorsIntent);
-            }
-        });
-
-
-        TextView phrases = (TextView)findViewById(R.id.phrases);
-        //Set clickListener on that View
-        phrases.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View view){
-                Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
-                startActivity(phrasesIntent);
-            }
-        });
-
-
-
+        // Connect the tab layout with the view pager. This will
+        //   1. Update the tab layout when the view pager is swiped
+        //   2. Update the view pager when a tab is selected
+        //   3. Set the tab layout's tab names with the view pager's adapter's titles
+        //      by calling onPageTitle()
+        tabLayout.setupWithViewPager(viewPager);
     }
-
-    /*public void openNumberList(View view){
-        Intent i = new Intent(this, NumbersActivity.class);
-        startActivity(i);
-    }*/
 }
